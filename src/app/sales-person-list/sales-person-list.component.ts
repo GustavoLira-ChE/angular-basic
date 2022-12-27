94b0c1f6ea70bf6be30841db9462a72ab18600d2
Gustavo Lira <gu.lirar23@gmail.com>
2022-12-27 12:08:36 -0600

    Add bbotstrap styles to table

--- src/app/sales-person-list/sales-person-list.component.ts
+++ src/app/sales-person-list/sales-person-list.component.ts
@@ -3,15 +3,15 @@ import { SalesPerson } from './sales-person';
 
 @Component({
   selector: 'app-sales-person-list',
-  templateUrl: './sales-person-list.component.html',
+  templateUrl: './sales-person-list-bootstrap.component.html',
   styleUrls: ['./sales-person-list.component.css']
 })
 export class SalesPersonListComponent implements OnInit {
 
   salesPersonList: SalesPerson[] = [
-    new SalesPerson("John","Doe","john@email.com", 500),
-    new SalesPerson("Claire","Murphy","claire@email.com",600),
-    new SalesPerson("Alan","Brito","alan@email.com",400)
+    new SalesPerson("John","Doe","john@email.com", 50000),
+    new SalesPerson("Claire","Murphy","claire@email.com",60000),
+    new SalesPerson("Alan","Brito","alan@email.com",40000)
   ];
   constructor(
   ){
